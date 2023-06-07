0779ca625d94a0524cf097d7858bc56084f789c2
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2023-06-07 19:40:07 -0400

    Initial Commit

---
+++ joyStemApp/joyStemAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  joyStemAppApp.swift
+//  joyStemApp
+//
+//  Created by Kiara on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct joyStemAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
