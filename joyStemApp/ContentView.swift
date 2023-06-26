09c4013fe603e5b3095af2305cecc860ea0f2da7
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2023-06-26 10:34:24 -0400

    Completed app - added stack, picture, and text

--- joyStemApp/ContentView.swift
+++ joyStemApp/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemPurple).ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("joy").resizable().aspectRatio(contentMode: .fit).cornerRadius(15)
+                
+                HStack(spacing: 40.0) {
+                    Text("Joy Buolamwini")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Text("Poet of Code")
+                }
+                
+                Text("Founder of the Algorithmic Justice League. Uses art and research to illuminate social implications of AI and to promote equitable and accountable technology.")
+            }
+            
+            .padding()
+            .background(Rectangle().foregroundColor(.white)).cornerRadius(15).shadow(radius: 15)
+            .padding()
         }
-        .padding()
     }
 }
 
